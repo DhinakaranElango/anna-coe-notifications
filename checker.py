fae674b03d945150b47afb7fe02589e0b46dff9c
DhinakaranElango <108855422+DhinakaranElango@users.noreply.github.com>
2026-09-15 15:15:57 +0530

    Update checker.py

--- checker.py
+++ checker.py
@@ -124,7 +124,7 @@ def parse_page(html, source):
         
         # Use the PDF filename when the website link says "Click Here"
     if title.lower() in {"click here", "clickhere"}:
-    filename = url.split("/")[-1].split("?")[0]
+        filename = url.split("/")[-1].split("?")[0]
     filename = re.sub(r"\.(pdf|docx?|xlsx?)$", "", filename, flags=re.I)
     filename = re.sub(r"[_-]+", " ", filename)
     filename = re.sub(r"\s+", " ", filename).strip()
